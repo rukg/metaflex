1f921bf20a6fb37664e2d4134eaabde5985be1bf
Спирин Владимир <spirinv92@gmail.com>
2021-12-01 23:20:16 +0300

    rename

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import {ArweaveData} from './types';
 
 
-async function getMetaAttributes(metadataAccount: string): Promise<ArweaveData> {
+async function getMetaData(metadataAccount: string): Promise<ArweaveData> {
 
     const connection = new Connection('devnet');
     const metaData = await programs.metadata.Metadata.load(connection, metadataAccount);
@@ -14,6 +14,6 @@ async function getMetaAttributes(metadataAccount: string): Promise<ArweaveData> 
 }
 
 
-getMetaAttributes('DXq59ovE9hLJWYnaP3r9PXss5aYA5xcp8avQz5FTWatV')
+getMetaData('DXq59ovE9hLJWYnaP3r9PXss5aYA5xcp8avQz5FTWatV')
     .then(data => console.log(data))
     .catch(error => console.log(error));
